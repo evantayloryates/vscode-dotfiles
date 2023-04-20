def1e22054b0354d2d6ba46d07cf8783efbcd7b8
Evan Taylor Yates <evantayloryates@gmail.com>
2023-04-19 21:28:25 -0700

    checkpoint

--- functions.sh
+++ functions.sh
@@ -15,7 +15,17 @@ gb() {
 
 
 gcm() {
-  local push=false message="checkpoint"
+  local push=false message="checkpoint" filelist=()
+
+  # Check if the current branch is master or production
+  local branch=$(git rev-parse --abbrev-ref HEAD)
+  if [[ $branch == "master" || $branch == "production" ]]; then
+    read -p "You are on the $branch branch. Proceed? (y/n) " confirm
+    if [[ $confirm != "y" && $confirm != "Y" ]]; then
+      echo "Exiting."
+      return 1
+    fi
+  fi
 
   # Parse command line arguments
   while [[ $# -gt 0 ]]; do
@@ -26,25 +36,38 @@ gcm() {
       -m|--message)
         message="$2"
         shift 2 ;;
+      -f|--files)
+        # Parse the list of files
+        while [[ $# -gt 1 && $2 != -* ]]; do
+          filelist+=("$2")
+          shift
+        done
+        # Add the files to Git
+        git add "${filelist[@]}"
+        ;;
       *)
         echo "Unknown option: $1"
         return 1 ;;
     esac
   done
 
-  # Exit if there are no changes
-  if [[ -z $(git status -s) ]]; then
-    echo "No changes to commit. Exiting."
-    return 0
+  # If no file list was specified, add all changes
+  if [[ ${#filelist[@]} -eq 0 ]]; then
+    # Exit if there are no changes
+    if [[ -z $(git status -s) ]]; then
+      echo "No changes to commit. Exiting."
+      return 0
+    fi
+    git add .
   fi
 
   # Perform Git commands
-  git add .
   git commit -m "$message"
   if $push; then
     git push
   fi
 }
+
 
 gps() {
   git push
--- install.sh
+++ install.sh
@@ -6,5 +6,3 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 # Concatenate the contents of functions.sh with ~/.bashrc and append the result to ~/.bashrc
 cat "${SCRIPT_DIR}/functions.sh" >> ~/.bashrc
 
-# Print a message to confirm that the function was added to the ~/.bashrc file
-echo "The gb function has been added to the ~/.bashrc file"
